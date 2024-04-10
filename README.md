6d08e4524570bb91c91e49f939f0c4dc0cc3be1e
Harvey <hmer101@mit.edu>
2024-04-10 18:33:33 -0400

    Arm connects and moves. Untuned as of yet

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # 2024-Spring-2.12
 2024 Spring 2.12 Final Project Team
+
+
+
+# UR Arm
+## Installation
+Install RTDE c++ library: https://sdurobotics.gitlab.io/ur_rtde/installation/installation.html#installation
+
+Install RTDE python client library: https://github.com/UniversalRobots/RTDE_Python_Client_Library/tree/main 
+
+
+## Useful references
+RTDE guide: https://www.universal-robots.com/articles/ur/interface-communication/real-time-data-exchange-rtde-guide/
--- main_arm_control.py
+++ main_arm_control.py
@@ -1,48 +1,116 @@
-import rtde.rtde as rtde
-import rtde.rtde_config as rtde_config
+import rtde_control, rtde_receive
+import time
+import keyboard 
 
-# Configuration
-ROBOT_IP = '192.168.1.10'
-ROBOT_PORT = 30004
-CONFIG_FILE = 'path/to/your/configuration_file.xml'
+# PARAMETERS
+# Robot IP address
+IP_UR5 = "169.254.157.0"
+USE_ROBOT = True
 
-# Target position: [X, Y, Z, RX, RY, RZ]
-# Note: Specify the target position here. Values are in meters and radians.
-target_position = [0.5, -0.2, 0.3, 0, 3.1415, 0]
+# Keyboard control directions and commands
+KEY_XM = 's'
+KEY_XP = 'f'
+KEY_YM = 'd'
+KEY_YP = 'e'
 
-# Setup RTDE communication and configuration
-conf = rtde_config.ConfigFile(CONFIG_FILE)
-output_names, output_types = conf.get_recipe('out')
-input_names, input_types = conf.get_recipe('in')
-con = rtde.RTDE(ROBOT_IP, ROBOT_PORT)
-con.connect()
+KEY_ACW = 'j'
+KEY_CW = 'l'
+KEY_ZP = 'i'
+KEY_ZM = 'k'
 
-# Setup output and input recipes
-con.send_output_setup(output_names, output_types)
-con.send_input_setup(input_names, input_types)
+KEY_QUIT = 'q'
 
-# Start RTDE data synchronization
-con.send_start()
+# Set position increments
+INC_PLANE = 0.01
+INC_HEIGHT = 0.01
+INC_ROT = 0.01
 
-# Create a new input packet to send to the robot
-input_data = con.send_input_setup(input_names, input_types)
+SPEED_L = 3.0 #0.5 #0.25
+ACCEL_L = 0.1 #25
 
-# Set the target position in the input packet
-input_data.input_double_register_0 = target_position[0]
-input_data.input_double_register_1 = target_position[1]
-input_data.input_double_register_2 = target_position[2]
-input_data.input_double_register_3 = target_position[3]
-input_data.input_double_register_4 = target_position[4]
-input_data.input_double_register_5 = target_position[5]
+# VARIABLES
+# Current position of the robot
+current_poseL_d = [0, 0, 0, 0, 0, 0]
 
-# Send the input packet to the robot to move the tool
-con.send(input_data)
 
-# Wait for the movement to complete
-# Note: Implement appropriate waiting or checking mechanism based on your application needs
+def setup():
+    # Connect to the robot
+    if USE_ROBOT:
+        # Connect to the robot
+        rtde_c =  rtde_control.RTDEControlInterface(IP_UR5)
+        rtde_r = rtde_receive.RTDEReceiveInterface(IP_UR5)
 
-# Stop RTDE data synchronization
-con.send_pause()
+        # Get the current position of the robot
+        current_poseL_d[:] = rtde_r.getActualTCPPose()
+    else:
+        rtde_c = None
+        rtde_r = None
 
-# Disconnect from the robot
-con.disconnect()
+        current_poseL_d[:] = [10, 10, 10, 10, 10, 10]
+
+    print('Setup complete. Robot connected.')
+
+    return rtde_c, rtde_r
+
+
+def loop(rtde_c, rtde_r):
+    ## POLLING LOOP
+    print('About to enter manual control loop. Press q to quit.')
+    
+    while True:
+        if keyboard.is_pressed(KEY_XM):
+            current_poseL_d[0] -= INC_PLANE
+
+        elif keyboard.is_pressed(KEY_XP):
+            current_poseL_d[0] += INC_PLANE
+
+        elif keyboard.is_pressed(KEY_YP):
+            current_poseL_d[1] += INC_PLANE
+
+        elif keyboard.is_pressed(KEY_YM):
+            current_poseL_d[1] -= INC_PLANE
+
+        elif keyboard.is_pressed(KEY_ZP):
+            current_poseL_d[2] += INC_HEIGHT
+
+        elif keyboard.is_pressed(KEY_ZM):
+            current_poseL_d[2] -= INC_HEIGHT
+
+        elif keyboard.is_pressed(KEY_ACW):
+            current_poseL_d[5] += INC_ROT
+
+        elif keyboard.is_pressed(KEY_CW):
+            current_poseL_d[5] -= INC_ROT
+        
+        elif keyboard.is_pressed(KEY_QUIT): 
+            print('Quit key pressed')
+            break  # finishing the loop
+
+        # Move to desired setpoint
+        # Move asynchronously in cartesian space to target, we specify asynchronous behavior by setting the async parameter to
+        # 'True'. Try to set the async parameter to 'False' to observe a default synchronous movement, which cannot be stopped
+        # by the stopL function due to the blocking behaviour.
+        rtde_c.moveL(current_poseL_d, SPEED_L, ACCEL_L, False)
+        print(current_poseL_d)
+
+        time.sleep(0.01) # Run at 100 Hz
+        
+
+if __name__ == "__main__":
+    # SETUP
+    rtde_c, rtde_r = setup()
+
+    # LOOP
+    loop(rtde_c, rtde_r)
+
+
+
+# Perhaps: move asynchronously parallel to windows of rubble until high IR detected and stop there? or store highest IR value and move back to that
+
+# USEFUL FUNCTIONS: 
+# rtde_c.moveUntilContact(speed)
+# moveJ - in joint space, moveL - in cartesian space
+# moveJ_IK - specify a pose that a robot will move to linearly in joint space
+# ***moveL - can set pos + speed + acceleration. Moves linearly in tool space
+#       - can specify "path" - 2d vector of doubles that also includes speeds and accelerations
+# ***movePath - move to each waypoint specified in a given path   
