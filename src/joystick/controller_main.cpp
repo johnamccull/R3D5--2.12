9210ec586d2e73bd538b8c08f2fec2ce13e4df31
otti0303 <otti03@mit.edu>
2024-05-02 18:03:57 -0400

    controller edits, joystick speed mode button

--- src/joystick/controller_main.cpp
+++ src/joystick/controller_main.cpp
@@ -13,6 +13,9 @@ ControllerMessage prevControllerMessage;
 
 Joystick joystick1(JOYSTICK1_X_PIN, JOYSTICK1_Y_PIN);
 Joystick joystick2(JOYSTICK2_X_PIN, JOYSTICK2_Y_PIN);
+#define V_PIN 8// speed mode
+// #define A_PIN 9 // autonomous mode
+#define SLOW_FACTOR 0.2
 
 void setup() {
     Serial.begin(115200);
@@ -23,15 +26,31 @@ void setup() {
     joystick2.setup();
 
     Serial.println("Setup complete.");
+    pinMode(V_PIN, INPUT_PULLUP);
+    // pinMode(A_PIN, INPUT_PULLUP);
 }
 
 void loop() {
     // Read and send controller sensors
+    
     EVERY_N_MILLIS(5) {
+        Serial.println(digitalRead(V_PIN));
+
+        // if high speed mode
+        
         controllerMessage.millis = millis();
         controllerMessage.joystick1 = joystick1.read();
         controllerMessage.joystick2 = joystick2.read();
         
+
+        //if low speed mode
+        if (!digitalRead(V_PIN)){
+            controllerMessage.joystick1.x *= SLOW_FACTOR;
+            controllerMessage.joystick1.y *= SLOW_FACTOR;
+            controllerMessage.joystick2.x *= SLOW_FACTOR;
+            controllerMessage.joystick2.y *= SLOW_FACTOR;
+        }
+
         
         if (!(prevControllerMessage == controllerMessage)) {
             sendControllerData();
