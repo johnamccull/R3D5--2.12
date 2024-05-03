acc83ec164a63a0d443b960461a256de3d999a96
John McCullough <144560738+johnmccu@users.noreply.github.com>
2024-05-03 15:39:40 -0400

    modified:   src/robot/robot_main.cpp 	modified:   src/temp/robot_motion_control.cpp 	modified:   include/robot_drive.h 	modified:   src/robot/robot_main.cpp

--- src/robot/robot_main.cpp
+++ src/robot/robot_main.cpp
@@ -27,7 +27,6 @@ void setup() {
 void loop() {
     // Update velocity setpoints based on trajectory at 50Hz
     EVERY_N_MILLIS(20) {
-        //followTrajectory();
         if (freshWirelessData) {
 
             //double forward = -mapDouble(controllerMessage.joystick1.x, -1, 1, -MAX_FORWARD, MAX_FORWARD);
@@ -39,44 +38,7 @@ void loop() {
             
             updateSetpoints(left, right);
             updateControlEffort();
+            
         }
-    }
-
-    // Update PID at 200Hz
-    /**
-    EVERY_N_MILLIS(5) {
-        updateControlEffort();
-    }
-    **/
-
-    // Send and print robot values at 200Hz
-    /**
-    EVERY_N_MILLIS(5) {
-        //updateOdometry();
-        // take angles from traction wheels only since they don't slip
-        currPhiL = encoders[2].getPosition();
-        currPhiR = -encoders[3].getPosition();
-        
-        double dPhiL = currPhiL - prevPhiL;
-        double dPhiR = currPhiR - prevPhiR;
-        prevPhiL = currPhiL;
-        prevPhiR = currPhiR;
-
-        float dtheta = r/(2*b)*(dPhiR-dPhiL);
-        float dx = r/2.0 * (cos(robotMessage.theta)*dPhiR + cos(robotMessage.theta)*dPhiL);
-        float dy = r/2.0 * (sin(robotMessage.theta)*dPhiR + sin(robotMessage.theta)*dPhiL);
-
-        // Update robot message 
-        robotMessage.millis = millis();
-        robotMessage.x += dx;
-        robotMessage.y += dy;
-        robotMessage.theta += dtheta;
-        
-        sendRobotData();
-
-        Serial.printf("x: %.2f, y: %.2f, theta: %.2f\n",
-                    robotMessage.x, robotMessage.y, robotMessage.theta);
-    }
-    **/
-  
+    }  
 }
