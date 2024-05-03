33e3c8d63e6e0fbfd4d1fcec434e1c8bd36b73c1
Elysia Yuan <yelysia@gmail.com>
2024-05-03 08:21:47 -0400

    correct signature and docstring of quadratic mapping function

--- lib/util/util.cpp
+++ lib/util/util.cpp
@@ -9,25 +9,27 @@ double mapDouble(double input_value, double in_min, double in_max, double out_mi
 }
 
 /**
- * map from an input to output range = quadratically,
- * piecewise using two parabolas that are functions of x, where x is a value in the output range,
- * and that both share a vertex at x = center value of the output range:
+ * map from an input to output range quadratically.
  * 
- * final output value = f(x) [upward sloping parabola] for x in the upper half of the output  (x >= center value of output range),
- *                      otherwise -f(x) for x in the lower half of the output range (x < center value of output range), 
+ * NOTE: input and output ranges both must be symmetric about 0.
+ * 
+ * map is piecewise using two parabolas that are functions of x, where x is a value in the output range:
+ * 
+ * final output value = f(x) [upward sloping parabola] for x in the upper half of the output  (x >= 0),
+ *                      otherwise -f(x) for x in the lower half of the output range (x < 0), 
  *    and f(x) is such that:
- *        vertex is f(x = center of output range) = 0
- *        f(x=out_max) = out_max 
- *        f(x=out_min) = out_min
+ *        vertex is f(x = 0) = 0
+ *        f(x=out_max) = abs(out_max)
+ *        f(x=out_min) = -abs(out_min)
  * 
  * input_value: original value in input range to map
- * in_min, in_max: lower and upper bounds of input range
- * out_min, out_max: lower and upper bounds of output range
+ * in_bound: positive value, lower and upper bounds of input range are -in_bound and in_bound respectively
+ * out_bound, out_max: positive value, lower and upper bounds of output range are -out_bound and out_bound respectively
  **/
-double quadraticMapDouble(double input_value, double in_min, double in_max, double out_min, double out_max) {
-  double out_range_size = out_max - out_min;
-  double out_range_center = out_min + out_range_size / 2;
-  double x = mapDouble(input_value, in_min, in_max, out_min, out_max);
+double quadraticMapDouble(double input_value, double in_bound, double out_bound) {
+  double out_range_size = out_bound * 2;
+  double out_range_center = -out_bound + out_range_size / 2;
+  double x = mapDouble(input_value, -in_bound, in_bound, -out_bound, out_bound);
   if (x < out_range_center) {
     return -pow((x - out_range_center), 2) / (out_range_size/2);
   } 
--- lib/util/util.h
+++ lib/util/util.h
@@ -5,7 +5,7 @@
 #include "EveryNMillis.h"
 
 double mapDouble(double input_value, double in_min, double in_max, double out_min, double out_max);
-double quadraticMapDouble(double input_value, double in_min, double in_max, double out_min, double out_max);
+double quadraticMapDouble(double input_value, double in_bound, double out_bound);
 void printTabs(uint8_t nTabs);
 
 #endif // UTIL_H
