b2cdb13617753375f7d16c660bac6624e8ec64fe
amymoha <amymohamed06@gmail.com>
2024-05-04 17:10:45 -0400

    tof

--- src/test/tof_test.cpp
+++ src/test/tof_test.cpp
@@ -1,14 +1,17 @@
 #include "Adafruit_VL53L0X.h"
+#define TCAADDR 0x70
 
 Adafruit_VL53L0X lox = Adafruit_VL53L0X();
+void tcaselect(uint8_t i);
 
 void setup() {
+  tcaselect(1);
   Serial.begin(115200);
 
-  // wait until serial port opens for native USB devices
-  while (! Serial) {
-    delay(1);
-  }
+  // // wait until serial port opens for native USB devices
+  // while (! Serial) {
+  //   delay(1);
+  // }
   
   Serial.println("Adafruit VL53L0X test");
   if (!lox.begin()) {
@@ -22,7 +25,7 @@ void setup() {
 
 void loop() {
   VL53L0X_RangingMeasurementData_t measure;
-    
+  
   Serial.print("Reading a measurement... ");
   lox.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
 
@@ -33,4 +36,12 @@ void loop() {
   }
     
   delay(100);
+}
+
+void tcaselect(uint8_t i) {
+  if (i > 7) return;
+ 
+  Wire.beginTransmission(TCAADDR);
+  Wire.write(1 << i);
+  Wire.endTransmission();  
 }
